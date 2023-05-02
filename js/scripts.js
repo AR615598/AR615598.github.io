d2df6fcbe511b28595ddbe0152647230d2b47731
AR615598 <66263198+AR615598@users.noreply.github.com>
2023-05-02 19:21:27 -0400

    FIxed contacts

--- js/scripts.js
+++ js/scripts.js
@@ -19,3 +19,17 @@ function resizeIframe(obj) {
   obj.style.height = obj.contentWindow.document.documentElement.scrollHeight + 'px';
 }
 
+
+// const form = document.querySelector('form');
+// const submitButton = form.querySelector('button[type="submit"]');
+
+// form.addEventListener('submit', (event) => {
+//   event.preventDefault();
+
+//   const formData = new FormData(form);
+//   const formDataObject = Object.fromEntries(formData.entries());
+//   const preFilledUrl = 'https://docs.google.com/forms/d/e/1FAIpQLSeaKSEJTk1DH42BKNxBxJNGflctu8-rRFrVvOpj4AGfJMkn4g/viewform?usp=pp_url&entry.399394357=' + encodeURIComponent(formDataObject['entry.399394357']) + '&entry.140554033=' + encodeURIComponent(formDataObject['entry.140554033']);
+// ;
+
+//   window.location.href = preFilledUrl;
+// });
